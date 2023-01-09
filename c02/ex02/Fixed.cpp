a5fe0e4453ec3f797523e1df7f6885edc0bbbd6f
bylkode <hafidsaadibk@gmail.com>
2023-01-09 14:16:50 -0500

    Module01 ex02, Overloading the <= operator implemented

--- c02/ex02/Fixed.cpp
+++ c02/ex02/Fixed.cpp
@@ -265,3 +265,16 @@ bool Fixed::operator>=(const Fixed &rsh) const
 	return (this->_fixedPointValue >= rsh.getRawBits());
 }
 
+/**
+ * @brief Overload the <= and compare two Fixed objects
+ *
+ * @param rsh The right hand side of the operator
+ * @return true The left hand side is less than or equal to the right hand side
+ * @return false The left hand side is not less than or equal to the right hand side
+ */
+bool Fixed::operator<=(const Fixed &rsh) const
+{
+	return (this->_fixedPointValue <= rsh.getRawBits());
+}
+
+
