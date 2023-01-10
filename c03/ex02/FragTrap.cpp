308688ca24105fc2830cb0b061a5dd308061245b
Hafid Saadi <hsaadi@student.42quebec.com>
2023-01-10 14:27:44 -0500

    Module03 ex02, FragTrap implemenation

---
+++ c03/ex02/FragTrap.cpp
@@ -0,0 +1,38 @@
+#include "FragTrap.hpp"
+
+/**
+ * @brief Construct a new Frag Trap:: Frag Trap object
+ *
+ */
+FragTrap::FragTrap() : ClapTrap()
+{
+    this->_hitPoints = 100;
+    this->_energyPoints = 100;
+    this->_attackDamage = 30;
+    std::cout << "FragTrap default constructor called" << std::endl;
+}
+
+/**
+ * @brief Construct a new Frag Trap:: Frag Trap object
+ *
+ * @param string name: The name of the FragTrap
+ * @return FragTrap& The instance created
+ */
+FragTrap::FragTrap(std::string name) : ClapTrap(name)
+{
+    this->_hitPoints = 100;
+    this->_energyPoints = 100;
+    this->_attackDamage = 30;
+    std::cout << "FragTrap " << this->_name << " created" << std::endl;
+}
+
+/**
+ * @brief Destroy the Frag Trap:: Frag Trap object
+ *
+ */
+FragTrap::~FragTrap()
+{
+    std::cout << "FragTrap " << this->_name << " destroyed" << std::endl;
+}
+
+
