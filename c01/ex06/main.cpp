0fadad5df7752123b01611090d10348f47c545fc
Hafid Saadi <hsaadi@student.42quebec.com>
2023-01-11 13:59:27 -0500

    Module01 ex06, Cleaned

--- c01/ex06/main.cpp
+++ c01/ex06/main.cpp
@@ -4,8 +4,8 @@ int main()
 {
     Harl harl;
     harl.complain("DEBUG");
-    // harl.complain("INFO");
-    // harl.complain("WARNING");
-    // harl.complain("ERROR");
+    harl.complain("INFO");
+    harl.complain("WARNING");
+    harl.complain("ERROR");
     return 0;
 }
