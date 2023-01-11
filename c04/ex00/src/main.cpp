e45f2551d007021577263fe2eefc35d2dc385448
Hafid Saadi <hsaadi@student.42quebec.com>
2023-01-11 05:34:55 -0500

    Module04 ex00,  Tests and main added

--- c04/ex00/src/main.cpp
+++ c04/ex00/src/main.cpp
@@ -1,20 +1,47 @@
 #include "../include/Animal.hpp"
-#include "../include/Dog.hpp"
 #include "../include/Cat.hpp"
+#include "../include/Dog.hpp"
+#include "../include/WrongAnimal.hpp"
+#include "../include/WrongCat.hpp"
 
-int main()
+void	test_animals(void)
 {
-    const Animal *meta = new Animal();
-    const Animal *j = new Dog();
-    const Animal *i = new Cat();
-    std::cout << j->getType() << " " << std::endl;
-    std::cout << i->getType() << " " << std::endl;
-    i->makeSound(); // will output the cat sound!
-    j->makeSound();
-    meta->makeSound();
-    delete meta;
-    delete j;
-    delete i;
+	const Animal	*meta;
+	const Animal	*j;
+	const Animal	*i;
+	delete			meta;
+	delete			j;
+	delete			i;
 
-    return 0;
+	meta = new Animal();
+	j = new Dog();
+	i = new Cat();
+	std::cout << "Type of meta = " << meta->getType() << std::endl;
+	std::cout << "Type of j = " << j->getType() << std::endl;
+	std::cout << "Type of i = " << i->getType() << std::endl;
+	i->makeSound();    // output: Cat says: Meow!
+	j->makeSound();    // output: Dog says: Woof!
+	meta->makeSound(); // output: Animals make a generic sound.
+}
+
+void	test_wrong_animals(void)
+{
+	const Animal	*wrong_meta;
+	const Animal	*wrong_i;
+	delete			wrong_meta;
+	delete			wrong_i;
+
+	wrong_meta = new WrongAnimal();
+	wrong_i = new WrongCat();
+	std::cout << "Type of wrong_meta = " << wrong_meta->getType() << std::endl;
+	std::cout << "Type of wrong_i = " << wrong_i->getType() << std::endl;
+	wrong_i->makeSound();    // output: WrongAnimals make a generic sound.
+	wrong_meta->makeSound(); // output: WrongAnimals make a generic sound.
+}
+
+int	main(void)
+{
+	test_animals();
+	test_wrong_animals();
+	return (0);
 }
