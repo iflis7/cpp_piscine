bb9b38d4ed41988699be232759d272af0483736c
Hafid Saadi <hsaadi@student.42quebec.com>
2023-01-11 06:08:14 -0500

    Module04 ex01,  Brain class implemented

--- c04/ex01/src/Brain.cpp
+++ c04/ex01/src/Brain.cpp
@@ -1,2 +1,44 @@
 #include "Brain.hpp"
 
+/**
+ * @brief Construct a new Brain:: Brain object
+ * 
+ */
+Brain::Brain()
+{
+	std::cout << "Brain constructor called" << std::endl;
+}
+
+/**
+ * @brief Construct a new Brain:: Brain object
+ * 
+ * @param copy The boject to copy
+ */
+Brain::Brain(const Brain &copy)
+{
+	std::cout << "Brain copy constructor called" << std::endl;
+	*this = copy;
+}
+
+/**
+ * @brief Copy the object
+ * 
+ * @param src The src object
+ * @return Brain& The object returned
+ */
+Brain &Brain::operator=(const Brain &src)
+{
+	std::cout << "Brain assignation operator called" << std::endl;
+	if (this == &src)
+		return (*this);
+	return (*this);
+}
+
+/**
+ * @brief Destroy the Brain:: Brain object
+ * 
+ */
+Brain::~Brain()
+{
+	std::cout << "Brain destructor called" << std::endl;
+}
