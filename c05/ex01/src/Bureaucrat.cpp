a97a3d6ff6dc44e60bbaa8429c7889de053db5bc
bylkode <hafidsaadibk@gmail.com>
2023-01-26 12:22:57 -0500

    Module05 -ex00- Tests added. Cleaned

---
+++ c05/ex01/src/Bureaucrat.cpp
@@ -0,0 +1,144 @@
+#include "../include/Bureaucrat.hpp"
+
+/**
+ * @brief Construct a new Bureaucrat:: Bureaucrat object
+ *
+ */
+Bureaucrat::Bureaucrat() : _name("Default"), _grade(150)
+{
+    std::cout << "Bureaucrat Created by Default!" << std::endl;
+}
+
+/**
+ * @brief Construct a new Bureaucrat:: Bureaucrat object
+ *
+ * @param name The name of
+ * @param grade The grade to initilized at
+ */
+Bureaucrat::Bureaucrat(const std::string &name, int grade) : _name(name)
+{
+    if (grade > 150)
+    {
+        throw GradeTooLowException();
+    }
+    else if (grade < 1)
+    {
+        throw GradeTooHighException();
+    }
+    _grade = grade;
+}
+
+/**
+ * @brief Construct a new Bureaucrat:: Bureaucrat object
+ *
+ * @param src
+ */
+Bureaucrat::Bureaucrat(Bureaucrat const &src)
+{
+    *this = src;
+    // return this;
+}
+
+/**
+ * @brief Overload = operator
+ *
+ * @param rhs The right hand side obj
+ * @return Bureaucrat&  The oj to return
+ */
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &rhs)
+{
+    if (this != &rhs)
+    {
+        this->_name = rhs._name;
+        this->_grade = rhs._grade;
+    }
+    return *this;
+}
+
+// void operator<<(std::ostream &COUT, Bureaucrat &bureaucrat)
+// {
+//     COUT << bureaucrat.getName();
+// }
+
+/**
+ * @brief Destroy the Bureaucrat:: Bureaucrat object
+ *
+ */
+Bureaucrat::~Bureaucrat(){
+
+};
+
+/**
+ * @brief Set the Name attribute
+ *
+ * @param name The name of the Bureaucrat
+ */
+void Bureaucrat::setName(const std::string name)
+{
+    this->_name = name;
+}
+
+/**
+ * @brief Get the name
+ *
+ * @return const std::string
+ */
+const std::string Bureaucrat::getName() const
+{
+    return (this->_name);
+};
+
+/**
+ * @brief Set the Grade attribute
+ *
+ * @param grade
+ */
+void Bureaucrat::setGrade(const int grade)
+{
+    this->_grade = grade;
+}
+
+/**
+ * @brief Get the grade
+ *
+ * @return int the grade
+ */
+int Bureaucrat::getGrade() const
+{
+    return (this->_grade);
+};
+
+/**
+ * @brief Increases the grade
+ *
+ */
+void Bureaucrat::incrementGrade()
+{
+    if (this->_grade <= 1)
+        throw GradeTooHighException();
+    this->_grade--;
+}
+
+/**
+ * @brief Decreases the grade
+ *
+ */
+void Bureaucrat::decrementGrade()
+{
+    if (this->_grade >= 150)
+        throw GradeTooLowException();
+    this->_grade++;
+}
+
+/**
+ * @brief
+ *
+ * @param out
+ * @param bureaucrat
+ * @return std::ostream&
+ */
+std::ostream &operator<<(std::ostream &out, const Bureaucrat &rhs)
+{
+    out << rhs.getName() << ", bureaucrat grade " << rhs.getGrade();
+    return (out);
+};
---
+++ c05/ex01/src/main.cpp
@@ -0,0 +1,76 @@
+#include "../include/Bureaucrat.hpp"
+
+int test1()
+{
+    Bureaucrat john("John", 2);
+    Bureaucrat jim("Jim", 149);
+
+    try
+    {
+        std::cout << john << std::endl;
+        std::cout << jim << std::endl;
+        john.incrementGrade();
+        jim.decrementGrade();
+        std::cout << john << std::endl;
+        std::cout << jim << std::endl;
+        john.incrementGrade();
+        jim.decrementGrade();
+        std::cout << john << std::endl;
+        std::cout << jim << std::endl;
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+    return (0);
+}
+
+int test2()
+{
+    try
+    {
+        Bureaucrat b("John Smith", 150);
+        std::cout << b << std::endl;
+        b.incrementGrade();
+        std::cout << b << std::endl;
+        b.decrementGrade();
+        b.decrementGrade();
+        std::cout << b << std::endl;
+    }
+    catch (Bureaucrat::GradeTooHighException &e)
+    {
+        std::cout << "Error: Grade is too high" << std::endl;
+    }
+    catch (Bureaucrat::GradeTooLowException &e)
+    {
+        std::cout << "Error: Grade is too low" << std::endl;
+    }
+    return 0;
+}
+
+int main()
+{
+    // test1();
+    test2();
+}
+
+/*
+// Let’s design an artificial nightmare of offices, corridors, forms, and waiting queues.
+// Sounds fun? No? Too bad.
+// First, start by the smallest cog in this vast bureaucratic machine: the Bureaucrat.
+// A Bureaucrat must have:
+// •A constant name.
+// •And a grade that ranges from 1 (highest possible grade) to 150 (lowest possible
+// grade).
+// Any attempt to instantiate a Bureaucrat using an invalid grade must throw an ex-
+// ception:
+// either a Bureaucrat::GradeTooHighException or a Bureaucrat::GradeTooLowException.
+// You will provide getters for both these attributes: getName() and getGrade(). Im-
+// plement also two member functions to increment or decrement the bureaucrat grade. If
+// the grade is out of range, both of them will throw the same exceptions as the constructor.
+// The thrown exceptions must be catchable using try and catch blocks:
+// You will implement an overload of the insertion («) operator to print something like
+// (without the angle brackets):
+// <name>, bureaucrat grade <grade>.
+// As usual, turn in some tests to prove everything works as expected.
+*/
