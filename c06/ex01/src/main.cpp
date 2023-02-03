351e0151edc370155fa9723879c12585f2ad7de8
Hafid Saadi <hsaadi@student.42quebec.com>
2023-02-03 16:21:01 -0500

    Module06 --Ex01--, Main Implemented

--- c06/ex01/src/main.cpp
+++ c06/ex01/src/main.cpp
@@ -1,1 +1,36 @@
+#include "../include/Data.hpp"
 
+uintptr_t serialize(Data *ptr)
+{
+    uintptr_t r;
+
+    r = reinterpret_cast<uintptr_t>(ptr);
+    return (r);
+}
+
+Data *deserialize(uintptr_t raw)
+{
+    Data *ptr;
+
+    ptr = reinterpret_cast<Data *>(raw);
+    return (ptr);
+}
+
+int main(void)
+{
+    std::cout << std::setw(30) << "==== ============ ====" << std::endl;
+
+    uintptr_t r;
+    Data d("Azul");
+    Data *ptr;
+
+    std::cout << std::setw(30) << "==== ====Test==== ====" << std::endl;
+
+    std::cout << "addr d   = " << &d << std::endl;
+    r = serialize(&d);
+    std::cout << "Value r = " << r << std::endl;
+    ptr = deserialize(r);
+    std::cout << "addr ptr = " << ptr << std::endl;
+
+    std::cout << "\n\t ============================== " << std::endl;
+}
