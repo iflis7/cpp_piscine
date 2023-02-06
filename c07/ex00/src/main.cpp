ad6b5f4f91e10f51dbab739538474f9245a08bb4
bylkode <hafidsaadibk@gmail.com>
2023-02-06 03:25:29 -0500

    Module07 --ex00--, Functions() implemented

--- c07/ex00/src/main.cpp
+++ c07/ex00/src/main.cpp
@@ -1,7 +1,18 @@
-#include "../include/main.hpp"
+#include "../include/Whatever.hpp"
 
-int main()
+int main(void)
 {
-    std::cout << "Hello, C07!" << std::endl;
-    return(0);
+    {
+        std::cout << std::setw(30) << " ============ ============ " << std::endl;
+        std::cout << std::setw(30) << " ========= Test Int ========= " << std::endl;
+        int x = 42.0f;
+        int y = 3.0f;
+
+        ::ft_putvar(x, y); // Check before the swap
+        ::swap(x, y);
+        ::ft_putvar(x, y); // Check after the swap
+        std::cout << "Min = " << ::min(x, y) << std::endl;
+        std::cout << "Max = " << ::max(x, y) << std::endl;
+    }
+    return (0);
 }
