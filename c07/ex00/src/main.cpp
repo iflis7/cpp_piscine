9547455c4f8e11354c5134c03800062a40238ccd
Hafid Saadi <hsaadi@student.42quebec.com>
2023-02-07 16:14:11 -0500

    Module07 --ex00--, Tests added and string test added

--- c07/ex00/src/main.cpp
+++ c07/ex00/src/main.cpp
@@ -1,59 +1,59 @@
 #define DOCTEST_CONFIG_IMPLEMENT
 #include "../include/Whatever.hpp"
 
-// TEST_CASE("Scalar Test Int Args")
-// {
-//     int x = 7;
-//     int y = 2;
+TEST_CASE("Scalar Test Int Args")
+{
+    int x = 7;
+    int y = 2;
 
-//     ::swap(x, y); // Swap
-//     CHECK(x == 2);
-//     CHECK(y == 7);
+    ::swap(x, y); // Swap
+    CHECK(x == 2);
+    CHECK(y == 7);
 
-//     ::swap(x, y); // Swap
-//     CHECK(x == 7);
-//     CHECK(y == 2);
+    ::swap(x, y); // Swap
+    CHECK(x == 7);
+    CHECK(y == 2);
 
-//     /* Max and Min values */
-//     CHECK(max(x, y) == 7);
-//     CHECK(min(x, y) == 2);
-// }
+    /* Max and Min values */
+    CHECK(max(x, y) == 7);
+    CHECK(min(x, y) == 2);
+}
 
-// TEST_CASE("Scalar Test Float Args")
-// {
-//     int x = 42.0f;
-//     int y = 3.0f;
+TEST_CASE("Scalar Test Float Args")
+{
+    int x = 42.0f;
+    int y = 3.0f;
 
-//     ::swap(x, y); // Swap
-//     CHECK(x == 3.0f);
-//     CHECK(y == 42.0f);
+    ::swap(x, y); // Swap
+    CHECK(x == 3.0f);
+    CHECK(y == 42.0f);
 
-//     ::swap(x, y); // Swap
-//     CHECK(x == 42.0f);
-//     CHECK(y == 3.0f);
+    ::swap(x, y); // Swap
+    CHECK(x == 42.0f);
+    CHECK(y == 3.0f);
 
-//     /* Max and Min values */
-//     CHECK(max(x, y) == 42.0f);
-//     CHECK(min(x, y) == 3.0f);
-// }
+    /* Max and Min values */
+    CHECK(max(x, y) == 42.0f);
+    CHECK(min(x, y) == 3.0f);
+}
 
-// TEST_CASE("Scalar Test Double Args")
-// {
-//     int x = 70.0;
-//     int y = 41.0;
+TEST_CASE("Scalar Test Double Args")
+{
+    int x = 70.0;
+    int y = 41.0;
 
-//     ::swap(x, y); // Swap
-//     CHECK(x == 41.0);
-//     CHECK(y == 70.0);
+    ::swap(x, y); // Swap
+    CHECK(x == 41.0);
+    CHECK(y == 70.0);
 
-//     ::swap(x, y); // Swap
-//     CHECK(x == 70.0);
-//     CHECK(y == 41.0);
+    ::swap(x, y); // Swap
+    CHECK(x == 70.0);
+    CHECK(y == 41.0);
 
-//     /* Max and Min values */
-//     CHECK(max(x, y) == 70.0);
-//     CHECK(min(x, y) == 41.0);
-// }
+    /* Max and Min values */
+    CHECK(max(x, y) == 70.0);
+    CHECK(min(x, y) == 41.0);
+}
 
 TEST_CASE("Scalar Test String Args")
 {
