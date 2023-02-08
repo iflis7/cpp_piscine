90531ea956a47fa39f7ff041bddcec8787061bd3
bylkode <hafidsaadibk@gmail.com>
2023-02-08 07:43:44 -0500

    Module08 --ex00--, Cleaned

--- c08/ex00/include/EasyFind.hpp
+++ c08/ex00/include/EasyFind.hpp
@@ -2,7 +2,53 @@
 
 #include "../../../doctest.hpp"
 #include <iostream>
-#include <iomanip>
+#include <vector>
+#include <algorithm>
 
-/****************************************/
 // template <typename T>
+// typename T::iterator easyfind(T &container, int target)
+// {
+//     auto it = std::find(container.begin(), container.end(), target);
+//     if (it == container.end())
+//     {
+//         throw std::runtime_error("Value not found");
+//     }
+//     return it;
+// }
+
+// template <typename T>
+// typename T::iterator easyfind_(T &container, int target)
+// {
+//     typename T::iterator it;
+//     for (it = container.begin(); it != container.end(); ++it)
+//     {
+//         if (*it == target)
+//         {
+//             break;
+//         }
+//     }
+//     if (it == container.end())
+//     {
+//         throw std::runtime_error("Value not found");
+//     }
+//     return it;
+// }
+
+template <typename T>
+typename T::iterator easyfind(T &container, int target)
+{
+    typename T::iterator it;
+    for(it = container.begin(); it != container.end(); ++it)
+    {
+        if(*it == target)
+        {
+            break;
+        }
+    }
+    if(it == container.end())
+    {
+        throw std::runtime_error("Value not found");
+    }   
+
+    return it;
+}
--- c08/ex00/src/main.cpp
+++ c08/ex00/src/main.cpp
@@ -1,13 +1,70 @@
 #define DOCTEST_CONFIG_IMPLEMENT
 #include "../include/EasyFind.hpp"
 
-TEST_CASE("Scalar Test Double Args")
+TEST_CASE("Test Int Args")
 {
-    int x = 70.0;
-    int y = 41.0;
+    int count = 10;
+    std::vector<int> v;
+    // for(int i = 0, j = 0; i < count; i++, j += 2)
+    // v.push_back(j);
+    for (int i = 0; i < count; i++)
+        v.push_back(i);
 
-    CHECK(x == 41.0);
-    CHECK(y == 70.0);
+    try
+    {
+        for (int i = 0; i < count; i++)
+        {
+            typename std::vector<int>::iterator it = easyfind(v, i);
+            CHECK(*it == i);
+        }
+    }
+    catch (const std::runtime_error &e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+}
+
+TEST_CASE("Test Int Args")
+{
+    int count = 10;
+    std::vector<int> v;
+    // for(int i = 0, j = 0; i < count; i++, j += 2)
+    // v.push_back(j);
+    for (int i = 0; i < count; i++)
+        v.push_back(i);
+
+    try
+    {
+        for (int i = 0; i < count; i++)
+        {
+            typename std::vector<int>::iterator it = easyfind(v, i);
+            CHECK(*it == i);
+        }
+    }
+    catch (const std::runtime_error &e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+}
+
+int test()
+{
+    std::cout << std::setw(30) << " ============ Test Value not found ============ " << std::endl;
+    std::vector<int> v;
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+
+    try
+    {
+        typename std::vector<int>::iterator it = easyfind(v, 4);
+        std::cout << *it << std::endl;
+    }
+    catch (const std::runtime_error &e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+    return 0;
 }
 
 int test1()
@@ -20,14 +77,15 @@ int main(int argc, char **argv)
 {
     // Including
     doctest::Context ctx;
-    ctx.setOption("abort-after", 5);  // default - stop after 5 failed asserts
+    ctx.setOption("abort-after", 5);  // default - stop after 5 failed asserts 
     ctx.applyCommandLine(argc, argv); // apply command line - argc / argv
     ctx.setOption("no-breaks", true); // override - don't break in the debugger
     int res = ctx.run();              // run test cases unless with --no-run
     if (ctx.shouldExit())             // query flags (and --exit) rely on this
         return res;                   // propagate the result of the tests
     std::cout << std::endl;
-    test1();
+    test();
+    // test1();
 
     return res; // + your_program_res
 }
