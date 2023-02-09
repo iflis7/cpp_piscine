17b7083961963cf447c8dcc2d790c5a55943a82b
bylkode <hafidsaadibk@gmail.com>
2023-02-09 10:04:59 -0500

    Module08 --ex02-- MustantStack implmented. Works with the provided main. Make more tests

--- c08/ex02/include/MutantStack.hpp
+++ c08/ex02/include/MutantStack.hpp
@@ -2,11 +2,54 @@
 
 #include "../../doctest.hpp"
 #include <iostream>
-#include <iomanip>
 #include <vector>
-#include <list>
 #include <stack>
 #include <algorithm>
 
+// template <typename T>
+// typename T::iterator easyfind(T &container, int target)
+// {
+//     typename T::iterator it;
+//     for (it = container.begin(); it != container.end(); ++it)
+//     {
+//         if (*it == target)
+//         {
+//             break;
+//         }
+//     }
+//     if (it == container.end())
+//     {
+//         throw std::runtime_error("Value not found");
+//     }
 
+//     return it;
+// }
 
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+public:
+    MutantStack() : std::stack<T>() {}
+    MutantStack(const MutantStack &other) : std::stack<T>(other) {}
+    MutantStack &operator=(const MutantStack &rhs)
+    {
+        if (this == &rhs)
+            return *this;
+        std::stack<T>::operator=(rhs);
+        return *this;
+    }
+    ~MutantStack() {}
+
+    typedef typename std::stack<T>::container_type::iterator iterator;
+
+    iterator begin()
+    {
+        return std::stack<T>::c.begin();
+    }
+    iterator end()
+    {
+        return std::stack<T>::c.end();
+    }
+
+    
+};
--- c08/ex02/src/main.cpp
+++ c08/ex02/src/main.cpp
@@ -1,78 +1,86 @@
 #define DOCTEST_CONFIG_IMPLEMENT
 #include "../include/MutantStack.hpp"
+#include "../include/MutantStack.hpp"
 
 int stackTest()
 {
+    // const int count = 5;
 
     std::cout << std::setw(30) << " ============ Test 1 ============ " << std::endl;
-    // MutantStack<int> mstack;
-    // mstack.push(5);
-    // mstack.push(17);
-    // std::cout << mstack.top() << std::endl;
-    // mstack.pop();
-    // std::cout << mstack.size() << std::endl;
-    // mstack.push(3);
-    // mstack.push(5);
-    // mstack.push(737);
-    // //[...]
-    // mstack.push(0);
-    // MutantStack<int>::iterator it = mstack.begin();
-    // MutantStack<int>::iterator ite = mstack.end();
-    // ++it;
-    // --it;
-    // while (it != ite)
+    MutantStack<int> a;
+    // a.printArr();
+
+    // std::cout << std::endl << std::setw(30) << " ============ Test Out Of Range ============ " << std::endl;
+    // Array<int> e;
+
+    // try
     // {
-    //     std::cout << *it << std::endl;
-    //     ++it;
+    //     std::cout << e[0] << std::endl;
     // }
-    // std::stack<int> s(mstack);
-    return 0;
+    // catch (const std::exception &e)
+    // {
+    //     std::cerr << e.what() << std::endl;
+    // }
+    return (0);
 }
 
-int listTest()
+int test1()
 {
-    std::cout << std::setw(30) << " ============ List Test ============ " << std::endl;
-    std::list<int> mlist;
-    mlist.push_back(5);
-    mlist.push_back(17);
-    std::cout << mlist.back() << std::endl;
-    mlist.pop_back();
-    std::cout << mlist.size() << std::endl;
-    mlist.push_back(3);
-    mlist.push_back(5);
-    mlist.push_back(737);
+    MutantStack<int> mstack;
+    mstack.push(5);
+    mstack.push(17);
+    std::cout << "mstack.top():: " << mstack.top() << std::endl;
+    mstack.pop();
+    std::cout << "mstack.size():: " << mstack.size() << std::endl;
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
     //[...]
-    mlist.push_back(0);
-    std::list<int>::iterator it = mlist.begin();
-    std::list<int>::iterator ite = mlist.end();
+    mstack.push(0);
+    MutantStack<int>::iterator it = mstack.begin();
+    std::cout << "mstack.begin():: " << *it << std::endl;
+    MutantStack<int>::iterator ite = mstack.end();
+    std::cout << "mstack.end()  :: " << *it << std::endl;
     ++it;
     --it;
     while (it != ite)
     {
         std::cout << *it << std::endl;
         ++it;
+        std::cout << *it << std::endl;
+        ++it;
     }
-
+    std::stack<int> s(mstack);
     return 0;
 }
 
 TEST_CASE("Array INT")
 {
-    // const int count = 5;
-
-    // Array<int> a(count);
-    // for (unsigned int i = 0; i < a.getSize(); i++)
-    //     a[i] = i;
-    // CHECK(a.getSize() == 5);
-    // CHECK(a[0] == 0);
-    // CHECK(a[2] == 2);
-    // CHECK(a[4] == 4);
-
     // for (unsigned int i = 0; i < a.getSize(); i++)
     //     a[i] = a[i] * 2;
     // CHECK(a[0] == 0);
     // CHECK(a[2] == 4);
     // CHECK(a[4] == 8);
+}
+
+TEST_CASE("STRING Array Tests")
+{
+    // const int count = 5;
+    // Array<std::string> c(count);
+    // c[0] = "Velkacem";
+    // c[1] = "Yuva";
+    // c[2] = "Vel3id";
+    // c[3] = "Qsam";
+    // c[4] = "Arezqi";
+    // CHECK(c.getSize() == 5);
+    // CHECK(c[0] == "Velkacem");
+    // CHECK(c[2] == "Vel3id");
+    // CHECK(c[4] == "Arezqi");
+    // for (unsigned int i = 0; i < c.getSize(); i++)
+    //     c[i] = c[i].append(" n at winat").data();
+    // CHECK(c[0] == "Velkacem n at winat");
+    // CHECK(c[2] == "Vel3id n at winat");
+    // CHECK(c[4] == "Arezqi n at winat");
 }
 
 int main(int argc, char **argv)
@@ -88,8 +96,8 @@ int main(int argc, char **argv)
     std::cout << std::endl;
     (void)argc;
     (void)argv;
-    listTest();
-    // stackTest();
+    // printableTests();
+    test1();
 
     return res; // + your_program_res
 }
