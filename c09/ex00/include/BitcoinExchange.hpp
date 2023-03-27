5c7bdeda43743cbe5327e9b92877f5d3d2b265a5
bylkode <hafidsaadibk@gmail.com>
2023-03-26 20:47:09 -0400

    C09 --Ex00-- getCsvInfo() added

--- c09/ex00/include/BitcoinExchange.hpp
+++ c09/ex00/include/BitcoinExchange.hpp
@@ -1,10 +1,34 @@
 #pragma once
 
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
+
 #include "../../doctest.hpp"
 #include <algorithm>
 #include <iostream>
 #include <vector>
 #include <list>
+#include <fstream>
+#include <sstream>
 
+class BitcoinExchange
+{
 
+private:
+    std::vector<int> m_prices;
+    std::list<int> m_pricesList;
 
+public:
+    BitcoinExchange(std::string file);
+    BitcoinExchange(const BitcoinExchange &rhs);
+    ~BitcoinExchange();
+    BitcoinExchange &operator=(const BitcoinExchange &rhs);
+    int maxProfit();
+    int maxProfitList();
+    bool validDateForamt();
+    bool validValueFormat();
+    bool validInput();
+    std::vector<std::pair<std::string, float> > getCsvInfo(std::string file);
+};
+
+#endif // BITCOINEXCHANGE_HPP
--- c09/ex00/src/BitcoinExchange.cpp
+++ c09/ex00/src/BitcoinExchange.cpp
@@ -1,10 +1,119 @@
-#pragma once
+#include "../include/BitcoinExchange.hpp"
 
-#include "../../doctest.hpp"
-#include <algorithm>
-#include <iostream>
-#include <vector>
-#include <list>
+BitcoinExchange::BitcoinExchange(std::string file)
+{
+    // std::cout << "BitcoinExchange::BitcoinExchange()" << std::endl;
+    std::cout << "file: " << file << std::endl;
+    std::vector<std::pair<std::string, float> > csvInfo = getCsvInfo(file);
 
+    std::cout << "file1: " << file << std::endl;
+    for (std::vector<std::pair<std::string, float> >::iterator it = csvInfo.begin(); it != csvInfo.end(); ++it)
+    {
+        std::cout << it->first << ": " << it->second << std::endl;
+    }
+}
 
+BitcoinExchange::~BitcoinExchange()
+{
+    // std::cout << "BitcoinExchange::~BitcoinExchange()" << std::endl;
+}
 
+BitcoinExchange::BitcoinExchange(const BitcoinExchange &rhs)
+{
+    // std::cout << "BitcoinExchange::BitcoinExchange(const BitcoinExchange &rhs)" << std::endl;
+    *this = rhs;
+}
+
+BitcoinExchange &BitcoinExchange::operator=(const BitcoinExchange &rhs)
+{
+    // std::cout << "BitcoinExchange::operator=(const BitcoinExchange &rhs)" << std::endl;
+    if (this != &rhs)
+    {
+        m_prices = rhs.m_prices;
+        m_pricesList = rhs.m_pricesList;
+    }
+    return *this;
+}
+
+// int BitcoinExchange::maxProfit()
+// {
+//     int maxProfit = 0;
+//     int minPrice = m_prices[0];
+//     int maxPrice = m_prices[0];
+//     for (int i = 0; i < (int)m_prices.size(); i++)
+//     {
+//         if (m_prices[i] < minPrice)
+//         {
+//             minPrice = m_prices[i];
+//             maxPrice = m_prices[i];
+//         }
+//         if (m_prices[i] > maxPrice)
+//         {
+//             maxPrice = m_prices[i];
+//         }
+//         if (maxPrice - minPrice > maxProfit)
+//         {
+//             maxProfit = maxPrice - minPrice;
+//         }
+//     }
+//     return maxProfit;
+// }
+
+// int BitcoinExchange::maxProfitList()
+// {
+//     int maxProfit = 0;
+//     int minPrice = m_pricesList.front();
+//     int maxPrice = m_pricesList.front();
+//     for (int i = 0; i < m_pricesList.size(); i++)
+//     {
+//         if (m_pricesList.front() < minPrice)
+//         {
+//             minPrice = m_pricesList.front();
+//             maxPrice = m_pricesList.front();
+//         }
+//         if (m_pricesList.front() > maxPrice)
+//         {
+//             maxPrice = m_pricesList.front();
+//         }
+//         if (maxPrice - minPrice > maxProfit)
+//         {
+//             maxProfit = maxPrice - minPrice;
+//         }
+//         m_pricesList.pop_front();
+//     }
+//     return maxProfit;
+// }
+
+// bool BitcoinExchange::validDateForamt()
+// {
+//     return true;
+// }
+
+// bool BitcoinExchange::validValueFormat()
+// {
+//     return true;
+// }
+
+// bool BitcoinExchange::validInput()
+// {
+//     return true;
+// }
+
+std::vector<std::pair<std::string, float> > BitcoinExchange::getCsvInfo(const std::string file)
+{
+    std::vector<std::pair<std::string, float> > csvInfo;
+    std::ifstream csvFile(file.c_str());
+    if (!csvFile.good())
+        std::cout << "File is good" << std::endl;
+    std::string line;
+    std::string date;
+    float value;
+    while (std::getline(csvFile, line))
+    {
+        std::stringstream ss(line);
+        std::getline(ss, date, ',');
+        ss >> value;
+        csvInfo.push_back(std::make_pair(date, value));
+    }
+    return csvInfo;
+}
